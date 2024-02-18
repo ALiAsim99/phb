b8749fc9027ff33191004b76aeecc6a5ef8700b0
AliAsim99 <ali.99asim@gmail.com>
2024-02-18 12:16:37 +0500

    typo

--- index.js
+++ index.js
@@ -10,45 +10,17 @@ app.use(express.json())
 app.use(cors())
 const Person=require('./models/person')
   
-  let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-        "id": 5,
-        "name": "Zary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-]
+ 
 
 app.get("/api/persons",(req,res)=>{
     Person.find({}).then(persons=>res.json(persons))
 })
 
-app.get('/info',(req,res)=>{
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>`)
-})
+
 app.get("/api/persons/:id",(req,res)=>{
     Person.findById(req.params.id)
     .then(person=>{
-        if(persons){
+        if(person){
         res.json(person)
     }else{
         res.status(404).end()
